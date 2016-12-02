026c85ae5cf433823dddc5b08329edd3d1d719ff
danny <dannywang@augmentum.com.cn>
2016-12-02 16:06:15 +0800

    feat(wechat-service): init project

---
+++ src/main/java/com/school/project/framework/SchoolApp.java
@@ -0,0 +1,13 @@
+package com.school.project.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+public class SchoolApp {
+	//application entry
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApp.class, args);
+    }
+}
